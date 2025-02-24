f7be84ba3c053ff5796cd222262e7118e18fd916
Bellp99 <anabekk726@gmail.com>
2025-02-23 22:31:47 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function generateExcuse() {
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    let action = ['ate', 'peed on', 'crushed', 'broke'];
+    let what = ['my homework', 'my phone', 'the car'];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+    
+    let excuse = `${who[Math.floor(Math.random() * who.length)]} 
+                  ${action[Math.floor(Math.random() * action.length)]} 
+                  ${what[Math.floor(Math.random() * what.length)]} 
+                  ${when[Math.floor(Math.random() * when.length)]}.`;
+    
+    document.getElementById("excuse").innerText = excuse;
+}
+
+window.onload = generateExcuse;
